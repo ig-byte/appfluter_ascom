75421923f4a32b99a32904d9507220eec0c665b5
ig-byte <gabriel.silva.aravena@gmail.com>
2023-09-30 02:25:06 -0300

    Creación de componentes
    - Creación de barra de navegación.
    - Creación de barra de resultados.

---
+++ lib/components/my_barresult.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+
+class ResultsBar extends StatelessWidget {
+  final String? jornada;
+  final String equipoIzquierdaImagen;
+  final String equipoDerechaImagen;
+  final String horaPartido;
+  final String estadio;
+
+  const ResultsBar({
+    super.key,
+    required this.jornada,
+    required this.equipoIzquierdaImagen,
+    required this.equipoDerechaImagen,
+    required this.horaPartido,
+    required this.estadio,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      child: Center(
+        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const SizedBox(
+                width: 25,
+              ),
+              Image.asset(
+                equipoIzquierdaImagen,
+                width: 65,
+                height: 50,
+              ),
+              Column(
+                children: [
+                  Container(
+                      height: 50,
+                      width: 80,
+                      color: const Color.fromRGBO(248, 162, 0, 1),
+                      child: Text(
+                        horaPartido,
+                        style: const TextStyle(color: Colors.white),
+                      )),
+                  Container(
+                      height: 25,
+                      width: 80,
+                      color: const Color.fromRGBO(208, 137, 3, 1),
+                      child: Text(
+                        estadio,
+                        style: const TextStyle(color: Colors.white),
+                      ))
+                ],
+              ),
+              Image.asset(
+                equipoDerechaImagen,
+                height: 50,
+              ),
+              const Padding(
+                padding: EdgeInsets.only(right: 10),
+                child: Icon(Icons.lock_clock),
+              ),
+            ],
+          ),
+        ]),
+      ),
+    );
+  }
+}
--- lib/screens/results_screen.dart
+++ lib/screens/results_screen.dart
@@ -1,5 +1,7 @@
+import 'package:appfluter_ascom/components/my_barresult.dart';
 import 'package:appfluter_ascom/components/my_navigationbar.dart';
 import 'package:flutter/material.dart';
+import 'package:toggle_list/toggle_list.dart';
 
 class ResultScreen extends StatelessWidget {
   const ResultScreen({super.key});
@@ -16,88 +18,128 @@ class ResultScreen extends StatelessWidget {
           style: TextStyle(fontSize: 18),
         ),
       ),
-      body: const Center(
-        child: SingleChildScrollView(
-          child: Expanded(
-            child: Column(
+      body: SingleChildScrollView(
+        child: Column(
+          //mainAxisAlignment: CrossAxisAlignment,
+          children: [
+            const SizedBox(
+                child: ToggleList(
+              shrinkWrap: true,
               children: [
-                SizedBox(
-                  height: 50,
-                  child: Text("Portada Resultados"),
-                ),
-                SizedBox(
-                  height: 50,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text("MIÉ 27"),
-                      Text("Ayer"),
-                      Text("VIE 29"),
-                    ],
+                ToggleListItem(
+                  title: Text('Portada Resultados'),
+                  content: Padding(
+                    padding: EdgeInsets.all(20),
+                    child: Text('Campeonato nacional Scotiabank'),
                   ),
-                ),
-                SizedBox(
-                  //height: 50,
-                  child: Center(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Row(
-                          children: [
-                            Text("JORNADA 7"),
-                          ],
-                        ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: [
-                            Text("Celta"),
-                            Text("Resultado"),
-                            Text("Alavez"),
-                            Text(">"),
-                          ],
-                        ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceAround,
-                          children: [
-                            Text("Celta"),
-                            Text("Resultado"),
-                            Text("Alavez"),
-                            Text(">"),
-                          ],
-                        ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: [
-                            Text("Celta"),
-                            Text("Resultado"),
-                            Text("Alavez"),
-                            Text(">"),
-                          ],
-                        ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text(""),
-                            Text("VER CALENDARIO"),
-                            Text("VER CLASIFICACIÓN"),
-                            Text(""),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-                SizedBox(
-                  height: 50,
-                  child: Text("Data 1"),
-                ),
-                SizedBox(
-                  height: 50,
-                  child: Text("Data 1"),
                 ),
               ],
+            )),
+            Container(
+              decoration: const BoxDecoration(
+                  color: Color.fromRGBO(242, 242, 242, 1),
+                  border: Border(
+                      bottom:
+                          BorderSide(color: Color.fromRGBO(194, 194, 194, 1)),
+                      top: BorderSide(
+                          color: Color.fromRGBO(194, 194, 194, 1), width: 2))),
+              height: 50,
+              child: const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 10),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "MIÉ 27",
+                      style: TextStyle(
+                        color: Color.fromRGBO(156, 156, 156, 1),
+                        fontSize: 12,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          "HOY",
+                          style: TextStyle(
+                              color: Color.fromRGBO(112, 112, 112, 1),
+                              fontWeight: FontWeight.bold,
+                              fontSize: 14),
+                        ),
+                        Text(
+                          "29 SEPT 2023",
+                          style: TextStyle(
+                            color: Color.fromRGBO(156, 156, 156, 1),
+                            fontSize: 12,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        )
+                      ],
+                    ),
+                    Text(
+                      "MIÉ 27",
+                      style: TextStyle(
+                        color: Color.fromRGBO(156, 156, 156, 1),
+                        fontSize: 12,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    )
+                  ],
+                ),
+              ),
             ),
-          ),
+            const ResultsBar(
+              jornada: null,
+              equipoIzquierdaImagen: 'lib/images/equipo1.png',
+              equipoDerechaImagen: 'lib/images/equipo2.png',
+              horaPartido: '7:00 PM',
+              estadio: 'Nacional',
+            ),
+            const ResultsBar(
+              jornada: null,
+              equipoIzquierdaImagen: 'lib/images/equipo1.png',
+              equipoDerechaImagen: 'lib/images/equipo2.png',
+              horaPartido: '7:00 PM',
+              estadio: 'Nacional',
+            ),
+            const ResultsBar(
+              jornada: null,
+              equipoIzquierdaImagen: 'lib/images/equipo1.png',
+              equipoDerechaImagen: 'lib/images/equipo2.png',
+              horaPartido: '7:00 PM',
+              estadio: 'Nacional',
+            ),
+            const ResultsBar(
+              jornada: null,
+              equipoIzquierdaImagen: 'lib/images/equipo1.png',
+              equipoDerechaImagen: 'lib/images/equipo2.png',
+              horaPartido: '7:00 PM',
+              estadio: 'Nacional',
+            ),
+            const ResultsBar(
+              jornada: null,
+              equipoIzquierdaImagen: 'lib/images/equipo1.png',
+              equipoDerechaImagen: 'lib/images/equipo2.png',
+              horaPartido: '7:00 PM',
+              estadio: 'Nacional',
+            ),
+            const ResultsBar(
+              jornada: null,
+              equipoIzquierdaImagen: 'lib/images/equipo1.png',
+              equipoDerechaImagen: 'lib/images/equipo2.png',
+              horaPartido: '7:00 PM',
+              estadio: 'Nacional',
+            ),
+            const SizedBox(
+              height: 50,
+              child: Text("Data 1"),
+            ),
+            const SizedBox(
+              height: 50,
+              child: Text("Data 1"),
+            ),
+          ],
         ),
       ),
     );
