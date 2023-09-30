11058cfe87b55e4e97b364cafb2bd839c1fa90ce
ig-byte <gabriel.silva.aravena@gmail.com>
2023-09-29 22:39:51 -0300

    Cambio de nombre de componente
    cambio de nombre de componente en navigationbar

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,9 @@ import 'package:appfluter_ascom/screens/login_screen.dart';
 import 'package:appfluter_ascom/screens/news_screen.dart';
 import 'package:appfluter_ascom/screens/results_screen.dart';
 import 'package:flutter/material.dart';
+/*
+Pagina principal
+*/
 
 void main() => runApp(const MyApp());
 
